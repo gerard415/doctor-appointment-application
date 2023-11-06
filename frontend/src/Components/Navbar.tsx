8af89481ddd6783112f07ddc00ec61bf7324b881
gerard415 <chinedumike85@gmail.com>
2023-11-06 11:10:50 +0100

    Navbar

--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -2,11 +2,21 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between'>
+    <div className=' flex justify-between items-center w-full '>
         <div>
             Medicare
         </div>
-        <button>
+        <div className='space-x-7 text-[13px] hidden md:inline'>
+          <button>Home</button>
+          <button>Services</button>
+          <button>Find a Doctor</button>
+          <button>Contact</button>
+        </div>
+        <div className='space-x-4 text-[13px] hidden md:inline'>
+          <button className='bg-blue-600 h-[25px] w-[80px] rounded-2xl text-white '>Log in</button>
+          <button>Sign up</button>
+        </div>
+        <button className='md:hidden'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
