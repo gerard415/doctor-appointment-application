7ad33459c053705780ee8e7edabaed2bb85d4b66
gerard415 <chinedumike85@gmail.com>
2023-11-06 16:21:36 +0100

    Services Page

---
+++ frontend/src/Pages/ServicesPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Services from '../Components/Services'
+
+const ServicesPage = () => {
+  return (
+    <div>
+        <Services/>
+    </div>
+  )
+}
+
+export default ServicesPage
