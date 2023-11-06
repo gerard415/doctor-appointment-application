0ca35ace200f0e935434f0ddf63b8e64ead6dc56
gerard415 <chinedumike85@gmail.com>
2023-11-06 11:13:03 +0100

    Landing Page layout

--- frontend/src/Components/LandingPageLayout.tsx
+++ frontend/src/Components/LandingPageLayout.tsx
@@ -5,8 +5,10 @@ import Footer from './Footer';
 
 const LandingPageLayout = () => {
   return (
-    <div className='max-w-screen min-h-screen py-7 px-4 phone:px-10 md:px-24 font-poppins overflow-auto space-y-10 flex flex-col justify-between'>
+    <div className='max-w-screen min-h-screen font-poppins space-y-10 flex flex-col justify-between phone:px-10 lg:px-24 '>
+      <div className='bg-white sticky top-0 w-full z-30 h-[70px] flex '>
         <Navbar/>
+      </div>
         <Outlet/>
         <Footer/>
     </div>
