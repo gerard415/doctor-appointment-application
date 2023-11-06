4004b18c64a95f72eebb1ebe803f673ff96d7502
gerard415 <chinedumike85@gmail.com>
2023-11-06 16:21:16 +0100

    Adding the other pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import LandingPageLayout from './Components/LandingPageLayout';
+import ServicesPage from './Pages/ServicesPage';
+import FindDoctorPage from './Pages/FindDoctorPage';
+import ContactPage from './Pages/ContactPage';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Routes>
         <Route path='/' element={<LandingPageLayout/>} >
           <Route index element={<HomePage/>} />
+          <Route path='/services' element={<ServicesPage/>} />
+          <Route path='/doctors' element={<FindDoctorPage/>} />
+          <Route path='/contact' element={<ContactPage/>} />
         </Route>
       </Routes>
     </>
