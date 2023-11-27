e1d86c345cc91678db68734e91a1da119f4c6b98
gerard415 <chinedumike85@gmail.com>
2023-11-27 10:25:44 +0100

    get doctor, get all doctors, edit and delete doctor routes

--- backend/controllers/doctor.ts
+++ backend/controllers/doctor.ts
@@ -1,19 +1,57 @@
 import {Request, Response} from 'express'
+import Doctor, {doctorType} from '../models/Doctor';
+import { StatusCodes } from 'http-status-codes';
+import BadRequestError from '../errors/bad-request';
+import UnauthenticatedError from '../errors/unauthenticated';
+import jwt, { Secret, JwtPayload } from 'jsonwebtoken';
+
+const SECRET: Secret = process.env.DOCTOR_SECRET!
+
+type MyToken = {
+    doctorId: number
+}
 
 const getDoctor = async (req: Request, res: Response) => {
-    res.send('get specific doctor')
+    const {token} = req.cookies
+
+    if(token) {
+        const {doctorId} = jwt.verify(token, SECRET) as MyToken
+        const {name, email, phone, role, appointments,reviews, _id:id} = await Doctor.findById(doctorId) as doctorType
+        res.status(StatusCodes.OK).json({name, email, phone, role, appointments, reviews, _id:id})
+    }else{
+        res.json(null)
+    }
 }
 
 const getAllDoctors = async (req: Request, res: Response) => {
-    res.send('get all doctors')
+    const doctors:doctorType[] = await Doctor.find().sort()
+    res.status(StatusCodes.OK).json({doctors, count: doctors.length})
 }
 
 const editDoctor = async (req: Request, res: Response) => {
-    res.send('edit doctor')
+    const {token} = req.cookies
+    const {name, password} = req.body
+
+    if(name === '' || password === ''){
+        throw new BadRequestError('Field cannot be empty')
+    }
+
+    if(token){
+        const {doctorId} = jwt.verify(token, SECRET) as MyToken
+        const user = await Doctor.findOneAndUpdate({_id: doctorId}, {...req.body}, {new:true, runValidators:true})
+        user?.save()
+        const {name, email, phone, role, appointments, _id:id} = user as doctorType
+        res.status(StatusCodes.OK).json({name, email, phone, role, appointments, _id:id})
+    }
 }
 
 const deleteDoctor = async (req: Request, res: Response) => {
-    res.send('delete doctor')
+    const {token} = req.cookies
+    if(token){
+        const {doctorId} = jwt.verify(token, SECRET) as MyToken
+        await Doctor.findByIdAndDelete(doctorId)
+        res.status(StatusCodes.OK).json('user successfully deleted')
+    }
 }
 
 const getDoctorBookings = async (req: Request, res: Response) => {
