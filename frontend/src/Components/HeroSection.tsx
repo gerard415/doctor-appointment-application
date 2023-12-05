a8f011db343b577fe2cc33bca1cf88a49484f70c
gerard415 <chinedumike85@gmail.com>
2023-12-05 07:42:57 +0100

    hero section setup

---
+++ frontend/src/Components/HeroSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div className='border border-black w-screen h-screen md:h-[500px]'>
+        Hero
+    </div>
+  )
+}
+
+export default HeroSection
