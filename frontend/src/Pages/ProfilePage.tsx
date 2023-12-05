2d81baf412143389d64cb7912ac3eb3f4d296c87
gerard415 <chinedumike85@gmail.com>
2023-12-05 07:47:04 +0100

    profile page and doctor dashboard setup

---
+++ frontend/src/Pages/ProfilePage.tsx
@@ -0,0 +1,43 @@
+import React,{useState, useContext} from 'react'
+import { Navigate } from "react-router-dom";
+import { UserContext } from '../UserContext'
+import { UserProps } from '../types'
+import axios from 'axios';
+
+const ProfilePage = () => {
+    const {setUser, user}: UserProps = useContext(UserContext)
+    const [redirect, setRedirect] = useState<boolean>(false)
+
+
+    const handleLogout = async () => {
+        await axios.post('/auth/logout')
+        setUser(null)
+        setRedirect(true)
+    }
+
+    const click = () => {
+        console.log(user)
+    }
+    if(redirect) {
+        return <Navigate to={'/'} />
+    }
+
+    return (
+        <div>
+            User Profile Page
+            
+            <div className='w-full h-[30px] items-center justify-center mb-10 '>
+                <button className='w-full pl-9 flex space-x-4 ' onClick={handleLogout} >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-5"><path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" /></svg>
+                    <span>Log Out</span>
+                </button>
+                <button className='w-full pl-9 flex space-x-4 ' onClick={click} >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-5"><path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" /></svg>
+                    <span>Click</span>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default ProfilePage
