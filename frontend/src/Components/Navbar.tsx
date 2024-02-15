03488299d75115c252b73c651818a64e68038115
gerard415 <82114246+gerard415@users.noreply.github.com>
2024-02-15 09:42:44 +0100

    miscellaneous changes

--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -49,8 +49,8 @@ const Navbar = () => {
                       <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                   </svg>
                 : 
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-5">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-5">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
                   </svg>
                 } 
               </button>
@@ -67,8 +67,8 @@ const Navbar = () => {
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
                : 
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-5">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-5">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
                 </svg>
               }
             </button>
--- frontend/src/Pages/SingleDoctorAboutPage.tsx
+++ frontend/src/Pages/SingleDoctorAboutPage.tsx
@@ -17,13 +17,13 @@ const SingleDoctorAboutPage = () => {
         <div className='space-y-10'>
             <div className='pt-7 space-y-4'>
                 <p className='text-[13px] font-semibold tracking-wide '>About of <span className='text-cyan-400 font-bold text-[15px] '>{doctor?.name}</span></p>
-                <p className='text-[12px] leading-7 text-gray-500 '>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+                <p className='text-[12px] leading-7 text-gray-500 '>{doctor?.about}</p>
             </div>
             <div>
                 <p className='text-[13px] font-semibold tracking-wide'>Education</p>
                 <div className='pt-2 space-y-4'>
                     {doctor?.qualifications?.map((qualification) => (
-                        <EducationCard qualification={qualification} />
+                        <EducationCard key={qualification.degree} qualification={qualification} />
                     ))}
                 </div>
             </div>
@@ -31,7 +31,7 @@ const SingleDoctorAboutPage = () => {
                 <p className='text-[13px] font-semibold tracking-wide'>Experience</p>
                 <div className='pt-2 grid grid-cols-2 gap-4'>
                     {doctor?.experiences?.map((experience) => (
-                        <ExperienceCard experience={experience}/>
+                        <ExperienceCard key={experience.startingDate} experience={experience}/>
                     ))}
                 </div>
             </div>
